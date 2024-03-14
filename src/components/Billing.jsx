36cd09a8f9dfeb483bfbe91c3c9389af68ff0919
Matthew James Collins <you@example.com>
2024-03-14 04:26:36 +0000

    Synced 14032024 - MJ COLLINS

---
+++ src/components/Billing.jsx
@@ -0,0 +1,38 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
+const Billing = () => (
+  <section id="products" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Download Zero-Flow, our traditional based web
+        payment integration system & the Web3 Zero-Crypto Wallet.
+        <br>
+        </br>
+        <br>        
+        </br>
+        Look out for our exclusive Airdrops, on Zero-Bank ICO.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Billing;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    // Add more file paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
